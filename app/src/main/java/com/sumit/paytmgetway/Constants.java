34db6b43493b4c1a56e580a9eafa55817fa228b9
sumit <sumit.pc@rediffmail.com>
2019-10-18 13:36:22 +0530

    Initial commit

---
+++ app/src/main/java/com/sumit/paytmgetway/Constants.java
@@ -0,0 +1,12 @@
+package com.sumit.paytmgetway;
+
+public class Constants {
+
+    public static final String M_ID = "ZsChlN43960031646677"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "WEBSTAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
---
+++ app/src/main/java/com/sumit/paytmgetway/MainActivity.java
@@ -0,0 +1,174 @@
+package com.sumit.paytmgetway;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.paytm.pgsdk.PaytmOrder;
+import com.paytm.pgsdk.PaytmPGService;
+import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity{
+
+    EditText orderid, custid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initOrderId();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        Button btn = (Button) findViewById(R.id.start_transaction);
+        orderid = (EditText) findViewById(R.id.orderid);
+        custid = (EditText) findViewById(R.id.custid);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, Checksum.class);
+//                intent.putExtra("orderid", orderid.getText().toString());
+//                intent.putExtra("custid", custid.getText().toString());
+//                startActivity(intent);
+                //calling the method generateCheckSum() which will generate the paytm checksum for payment
+                generateCheckSum();
+            }
+        });
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
+        }
+    }
+
+    private void generateCheckSum() {
+
+        //getting the tax amount first.
+        String txnAmount = String.valueOf(10).trim();
+
+        //creating a retrofit object.
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //creating the retrofit api service
+        Api apiService = retrofit.create(Api.class);
+
+        //creating paytm object
+        //containing all the values required
+        final Paytm paytm = new Paytm(
+                Constants.M_ID,
+                orderid.getText().toString(),
+                custid.getText().toString(),
+                Constants.CHANNEL_ID,
+                txnAmount,
+                Constants.WEBSITE,
+                Constants.CALLBACK_URL,
+                Constants.INDUSTRY_TYPE_ID
+        );
+
+        //creating a call object from the apiService
+        Call<Checksum> call = apiService.getChecksum(
+                paytm.getmId(),
+                paytm.getOrderId(),
+                paytm.getCustId(),
+                paytm.getChannelId(),
+                paytm.getTxnAmount(),
+                paytm.getWebsite(),
+                paytm.getCallBackUrl(),
+                paytm.getIndustryTypeId()
+        );
+
+        //making the call to generate checksum
+        call.enqueue(new Callback<Checksum>() {
+            @Override
+            public void onResponse(Call<Checksum> call, Response<Checksum> response) {
+
+                //once we get the checksum we will initiailize the payment.
+                //the method is taking the checksum we got and the paytm object as the parameter
+                initializePaytmPayment(response.body().getChecksumHash(), paytm);
+            }
+
+            @Override
+            public void onFailure(Call<Checksum> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+    private void initializePaytmPayment(String checksumHash, Paytm paytm) {
+
+        //getting paytm service
+        PaytmPGService Service = PaytmPGService.getStagingService();
+
+        //use this when using for production
+        //PaytmPGService Service = PaytmPGService.getProductionService();
+
+        //creating a hashmap and adding all the values required
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("MID", Constants.M_ID);
+        paramMap.put("ORDER_ID", paytm.getOrderId());
+        paramMap.put("CUST_ID", paytm.getCustId());
+        paramMap.put("CHANNEL_ID", paytm.getChannelId());
+        paramMap.put("TXN_AMOUNT", paytm.getTxnAmount());
+        paramMap.put("WEBSITE", paytm.getWebsite());
+        paramMap.put("CALLBACK_URL", paytm.getCallBackUrl());
+        paramMap.put("CHECKSUMHASH", checksumHash);
+        paramMap.put("INDUSTRY_TYPE_ID", paytm.getIndustryTypeId());
+
+
+        //creating a paytm order object using the hashmap
+        PaytmOrder order = new PaytmOrder(paramMap);
+
+        //intializing the paytm service
+        Service.initialize(order, null);
+
+        //finally starting the payment transaction
+        //Service.startPaymentTransaction(this, true, true, this);
+        Service.startPaymentTransaction(this, true, true, new PaytmPaymentTransactionCallback() {
+            /*Call Backs*/
+            public void someUIErrorOccurred(String inErrorMessage) {
+                Toast.makeText(getApplicationContext(), "UI Error " + inErrorMessage , Toast.LENGTH_LONG).show();
+            }
+            public void onTransactionResponse(Bundle inResponse) {
+                Toast.makeText(getApplicationContext(), "Payment Transaction response " + inResponse.toString(), Toast.LENGTH_LONG).show();
+            }
+            public void networkNotAvailable() {
+                Toast.makeText(getApplicationContext(), "Network connection error: Check your internet connectivity", Toast.LENGTH_LONG).show();
+            }
+            public void clientAuthenticationFailed(String inErrorMessage) {
+                Toast.makeText(getApplicationContext(), "Authentication failed: Server error" + inErrorMessage.toString(), Toast.LENGTH_LONG).show();
+            }
+            public void onErrorLoadingWebPage(int iniErrorCode, String inErrorMessage, String inFailingUrl) {
+                Toast.makeText(getApplicationContext(), "Unable to load webpage " + inErrorMessage.toString(), Toast.LENGTH_LONG).show();
+            }
+            public void onBackPressedCancelTransaction() {
+                Toast.makeText(getApplicationContext(), "Transaction cancelled" , Toast.LENGTH_LONG).show();
+            }
+            public void onTransactionCancel(String inErrorMessage, Bundle inResponse) {}
+        });
+
+    }
+
+
+}
